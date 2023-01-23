bcf3b1bf8e6e10d64a7bd37e06bf2a7874eadfbc
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-01-23 14:19:47 -0300

    feat: limpando arquivos gerados e configurando roteamento

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { RoutesApp } from "./routes";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <RoutesApp />
+    </BrowserRouter>
+  );
+}
