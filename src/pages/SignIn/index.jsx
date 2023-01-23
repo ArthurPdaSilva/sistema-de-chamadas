f5f371cb29b7fb360e25c3e149e8bbf92b81f074
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-01-23 14:39:46 -0300

    feat: estilizando tela de login

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import "./signin.css";
 
 export default function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>SignIn</div>
-  )
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamadas" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@gmail.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="********"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
 }
